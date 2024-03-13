a8c2a515d1de3812d1df30b683f7e43e3193979c
arttrip <162640219+arttrip@users.noreply.github.com>
2024-03-14 00:07:15 +0200

    Update plot3.C

--- plot3.C
+++ plot3.C
@@ -7,48 +7,35 @@
 void plot3(){
   // Open the input files:
   TFile *fsig=new TFile("histos_a20.root");
-  //TFile *fsig2=new TFile("histos_ana20.root");
-  TFile *fDY=new TFile("histos_anadyzvv.root");
-  // Get Histograms from signal file:
   
-  TH1F *hsig_mn_eta = new TH1F("","",100,0,8);
-  hsig_mn_eta=(TH1F*)fsig->Get("fjet_mult_after2");
- // hsig_mn_eta->Rebin(2);
  
-  TH1F *hbkg_mn_eta=new TH1F("","",100,80,100);
-  hbkg_mn_eta= (TH1F*)fDY->Get("fjet_mult_after2");
-//  hbkg_mn_eta->Rebin(2);
+  TH1F *hsig =(TH1F*)fsig->Get("sub_mult1");
+ 
+  TH1F *hbkg= (TH1F*)fsig->Get("sub_mult1_mat");
 
- // TH1F *hsig2_mn_eta=new TH1F("","",100,80,100);
- // hsig2_mn_eta= (TH1F*)fsig2->Get("jet_cc_mult3");
-  //hsig2_mn_eta->Rebin(4);
   // Draw the histos
   TCanvas *c1 = new TCanvas ("c1","c1",900,800);
-  hbkg_mn_eta->SetLineColor(kBlack);
-  hbkg_mn_eta->GetXaxis()->SetTitle("no of fjets");
- // hbkg_mn_eta->GetXaxis()->SetRangeUser(0,150);
-  hbkg_mn_eta->GetYaxis()->SetTitle("Entries");
-  hbkg_mn_eta->SetTitle("fjet mult (step 3 of the cut flow)");
-  hbkg_mn_eta->Draw("hist");
-  hbkg_mn_eta->Scale(1./hbkg_mn_eta->Integral());// Normalize distribution to 1
-  
- // hsig_mn_eta->SetLineColor(kRed);
- // hsig_mn_eta->Draw("histsames");
- // hsig_mn_eta->Scale(1./hsig_mn_eta->Integral());
+  c1->SetLogy();
+  hbkg->SetLineColor(kBlack);
+  hbkg->GetXaxis()->SetTitle("no of sub-jets");
+  hbkg->GetYaxis()->SetTitle("Entries");
+  hbkg->SetTitle("sub-jet mult in fjet1");
+  hbkg->Draw("hist");
+  hbkg->Scale(1./hbkg->Integral());// Normalize distribution to 1
 
-  hsig_mn_eta->SetLineColor(kRed);
-  hsig_mn_eta->Draw("histsames");
-  hsig_mn_eta->Scale(1./hsig_mn_eta->Integral()); // Normalize distribution to 1
+
+  hsig->SetLineColor(kRed);
+  hsig->Draw("histsames");
+  hsig->Scale(1./hsig->Integral()); // Normalize distribution to 1
   
  
   
  
   TLegend *leg = new TLegend(0.65,0.7,.9,.5);
  
-  leg->AddEntry(hsig_mn_eta,"ZH(m20) signal ","l");
+  leg->AddEntry(hsig,"fjet1 ","l");
   
-  leg->AddEntry(hbkg_mn_eta,"z->vv","l");
-  // leg->AddEntry(hsig2_mn_eta," step 3","l");
+  leg->AddEntry(hbkg,"fjet1 matched","l");
   leg->SetTextSize(0.04); 
   leg->Draw();
 		       
