097256938dc7865d0bd2e60827b154bba24d7b3d
arttrip <162640219+arttrip@users.noreply.github.com>
2024-03-26 12:01:19 +0200

    Add files via upload

--- ht.C
+++ ht.C
@@ -8,24 +8,27 @@
 void ht(){
   // Open the input files:
   TFile *fsig=new TFile("histos_a20.root");
-  TFile *fbkg=new TFile("histos_zvv.root");
+  TFile *fbkg1=new TFile("histos_zvv100.root");
+  TFile *fbkg2=new TFile("histos_zvv200.root");
 
   //CROS CLEANED jet mult in step 2
   
   TH1F *hjs1 =(TH1F*)fsig->Get("Ht");
-  TH1F *hjb1= (TH1F*)fbkg->Get("Ht");
+  TH1F *hjb1= (TH1F*)fbkg1->Get("Ht");
+  TH1F *hjb2= (TH1F*)fbkg2->Get("Ht");
+  hjb1->Add(hjb2);
   setTDRStyle();
   TCanvas *cas1 = new TCanvas ("cas1","cas1",900,800);
-  hjb1->SetLineColor(kBlack);
+  hjb1->SetLineColor(kRed);
   hjb1->GetXaxis()->SetTitle("H_{T} (GeV)");
   hjb1->GetYaxis()->SetTitle("Entries");
   hjb1->SetTitle("H_{T} ");
   hjb1->Draw("hist");
-  hjb1->SetFillColor(1);
+  hjb1->SetFillColor(kRed);
   hjb1->SetFillStyle(3004);
   hjb1->Scale(1./hjb1->Integral());// Normalize distribution to 1
 
-  hjs1->SetLineColor(kRed);
+  hjs1->SetLineColor(kBlack);
   hjs1->Draw("histsames");
   hjs1->SetLineWidth(2);
   hjs1->Scale(1./hjs1->Integral()); // Normalize distribution to 1
